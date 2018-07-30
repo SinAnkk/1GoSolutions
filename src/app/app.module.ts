067fcabb760dd6634c233e04659cd4f5d00dd240
ankur singh <writetoankursingh@gmail.com>
2018-07-30 20:05:32 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { JobSeekerComponent } from './job-seeker/job-seeker.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about',      component: AboutUsComponent },
+  { path: 'services',      component: ServicesComponent },
+  { path: 'contact',      component: ContactComponent },
+  { path: 'jobSeeker',      component: JobSeekerComponent },
+
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ServicesComponent,
+    ContactComponent,
+    JobSeekerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
