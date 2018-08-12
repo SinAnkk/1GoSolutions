96d61c6025526e9a2a88de3fc0178cef2d0c70da
unknown <writetoankursingh@gmail.com>
2018-08-12 11:31:07 -0700

    added candidate model

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+                                   
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,5 +11,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
   }
+
+
 
 }
---
+++ src/app/models/candidate.model.ts
@@ -0,0 +1,12 @@
+export class Candidate {
+    designation: string;
+    email: string;
+    experience: string;
+    id: 0;
+    industry: string;
+    location: string;
+    mobile: string;
+    name: string;
+    organisation: string;
+    salary: string;
+}
