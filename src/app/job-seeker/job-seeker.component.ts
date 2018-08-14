f2cebbb69ff886240a9a5a60d9e43d2377285532
unknown <writetoankursingh@gmail.com>
2018-08-14 23:57:27 +0530

    saveCandidate

--- src/app/job-seeker/job-seeker.component.ts
+++ src/app/job-seeker/job-seeker.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Candidate } from '../models/candidate.model';
 
 @Component({
   selector: 'app-job-seeker',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./job-seeker.component.css']
 })
 export class JobSeekerComponent implements OnInit {
+  candidate: Candidate = {
+    designation: null,
+    email: null,
+    experience:null,
+    id: null,
+    industry: null,
+    location: null,
+    mobile: null,
+    name: null,
+    organisation: null,
+    salary: null
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  saveCandidate(newCandidate: Candidate){
+    console.log(newCandidate)
   }
 
 }
